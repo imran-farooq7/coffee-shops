f620a901cf9c4f4cd70fac155bde1b6e03632784
imran farooq <imran_farooq7@hotmail.com>
2022-01-21 03:18:19 +0500

    FourSquare API connected

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../styles/Card.module.css";
 const Card = ({ image, title, href }) => {
 	return (
 		<Link href={href}>
-			<a>
-				<h2>{title}</h2>
-				<Image src={image} alt="coffee" width={260} height={160} />
+			<a className={styles.cardLink}>
+				<div className={`styles.container + glass`}>
+					<div className={styles.cardHeaderWrapper}>
+						<h2 className={styles.cardHeader}>{title}</h2>
+					</div>
+					<div className={styles.cardImageWrapper}>
+						<Image
+							className={styles.cardImage}
+							src={image}
+							alt="coffee"
+							width={260}
+							height={160}
+							objectFit="cover"
+						/>
+					</div>
+				</div>{" "}
 			</a>
 		</Link>
 	);
--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,35 +1,89 @@
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import Stores from "../../stores.json";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Coffe-store.module.css";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import StarsIcon from "@mui/icons-material/Stars";
+import PlaceIcon from "@mui/icons-material/Place";
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
 export async function getStaticProps({ params }) {
-	console.log("params ", params);
+	const coffeeStores = await fetchCoffeeStores();
 	return {
 		props: {
-			CoffeeStores: Stores.find((store) => {
-				return store.id.toString() === params.id;
+			CoffeeStores: coffeeStores.find((store) => {
+				return store.id === params.id.toString();
 			}),
 		},
 	};
 }
 export async function getStaticPaths() {
+	const coffeeStores = await fetchCoffeeStores();
+	const paths = coffeeStores.map((store) => {
+		return {
+			params: {
+				id: store.id,
+			},
+		};
+	});
 	return {
-		paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+		paths,
 		fallback: false,
 	};
 }
 const CoffeeStore = (props) => {
 	const router = useRouter();
 	const query = router.query;
+	if (router.isFallback) {
+		return <div>Loading...</div>;
+	}
+	const handleUpvote = () => {
+		console.log("clicked");
+	};
+	const { address, name, imgUrl } = props.CoffeeStores;
 	return (
-		<div>
-			<Link href="/">
-				<a>Back to Home</a>
-			</Link>
-			<Link href="/coffee-store${query}">
-				<a>dynamic</a>
-			</Link>
-			<h1>{props.CoffeeStores.name}</h1>
+		<div className={styles.layout}>
+			<Head>
+				<title>{name}</title>
+			</Head>
+			<div className={styles.container}>
+				<div className={styles.col1}>
+					<div className={styles.backToHomeLink}>
+						<Link href="/">
+							<a>Back to Home</a>
+						</Link>
+					</div>
+
+					<div className={styles.nameWrapper}>
+						<h1 className={styles.name}>{name}</h1>
+					</div>
+					<Image
+						src={
+							imgUrl ||
+							"https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+						}
+						width={600}
+						height={360}
+						className={styles.storeImg}
+					/>
+				</div>
+				<div className={`glass ${styles.col2}`}>
+					<div className={styles.iconWrapper}>
+						<PlaceIcon className={styles.icon} />
+						<p className={styles.text}>{address}</p>
+					</div>
+
+					<div className={styles.iconWrapper}>
+						<StarsIcon className={styles.icon} />
+						<p className={styles.text}>{1}</p>
+					</div>
+					<button className={styles.upvoteButton} onClick={handleUpvote}>
+						Up vote
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
